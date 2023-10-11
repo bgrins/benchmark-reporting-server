6c44d53b4a0b258fd43d37332ccb0b78951f5532
Brian Grinstead <briangrinstead@gmail.com>
2023-10-11 15:58:34 -0700

    get json

--- setup.sh
+++ setup.sh
@@ -3,6 +3,12 @@ gcloud config set project benchmark-results
 bq show speedometer3 || bq mk speedometer3
 bq show speedometer3.submission || bq mk --table speedometer3.submission name:STRING,score:FLOAT,useragent:STRING,client_hash:STRING,url:STRING,created:TIMESTAMP,notes:STRING,json:STRING,csv:STRING
 
+gcloud functions deploy getJson \
+  --source=functions \
+  --runtime=nodejs18 \
+  --trigger-http \
+  --allow-unauthenticated
+
 gcloud functions deploy insertData \
   --source=functions \
   --runtime=nodejs18 \
